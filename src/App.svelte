01d2ad83d0bcff9a2457ed685e08bb5d099faa50
jakazzy <jakazzy@gmail.com>
2019-09-29 13:42:38 +0000

    add items to project

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script>
-	export let name;
+  export let items = [
+    { id: 1, name: "milk", done: false },
+    { id: 2, name: "bread", done: true },
+    { id: 3, name: "sugar", done: false }
+  ];
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<div>
+  <h1>Things to buy</h1>
+
+  <ul>
+    {#each items as item}
+      <li>{item.name}</li>
+    {/each}
+  </ul>
+</div>
